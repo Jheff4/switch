ee087fb293006856e4d28fe9f2050201aa6b8e6d
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2022-11-18 22:31:26 +0100

    passed the request props to the buttons

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,23 @@
+import Button from "./Button"
+
 const Form = ({ reqType, setReqType }) => {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
-      
+      <Button 
+        buttonText="users"
+        reqType={reqType}
+        setReqType={setReqType}
+      />
+      <Button 
+        buttonText="users"
+        reqType={reqType}
+        setReqType={setReqType}
+      />
+      <Button 
+        buttonText="users"
+        reqType={reqType}
+        setReqType={setReqType}
+      />
     </form>
   )
 }
